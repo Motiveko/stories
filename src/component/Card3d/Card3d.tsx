710896943e882ca131928c8f9c603b3a5d9c4dd6
motiveko <rhehdrla@naver.com>
2022-08-02 23:07:26 +0900

    코드 정리

--- src/component/Card3d/Card3d.tsx
+++ src/component/Card3d/Card3d.tsx
@@ -3,7 +3,7 @@ import React, {useRef} from 'react';
 import styled from 'styled-components';
 import img from '../Card3d/background.jpg';
 
-const TILT_DEG = 25;
+const TILT_DEGREE = 25;
 const Frame = styled.div`
   width: 300px;
   height: 450px;
@@ -18,8 +18,8 @@ const Card = styled.div<CradProps>`
   border-radius: 6px;
   width: 100%;
   height: 100%;
-  transition: transform 60ms;
-  transform: rotate3d(var(--tiltX), var(--tiltY), 0, var(--deg));
+  transition: transform 110ms;
+  transform: rotate3d(var(--tiltX), var(--tiltY), 0, var(--tiltDegree));
   box-shadow: var(--shadowX) var(--shadowY) 10px rgba(0, 0, 0, 0.15);
   overflow: hidden;
   position: relative;
@@ -39,44 +39,61 @@ const Light = styled.div`
     #ffffff30
   ); ;
 `;
+const initCssVariables = (element: HTMLElement) => {
+  element.style.setProperty('--tiltX', '0');
+  element.style.setProperty('--tiltY', '0');
+  element.style.setProperty('--tiltDegree', '0');
+  element.style.setProperty('--shadowX', '0px');
+  element.style.setProperty('--shadowY', '0px');
+  element.style.setProperty('--centerX', '0px');
+  element.style.setProperty('--centerY', '0px');
+};
+
+const getPytagoras: (x: number, y: number) => number = (x, y) =>
+  Math.sqrt(x * x + y * y);
 
 const Card3d: React.FC = () => {
   const card = useRef<HTMLDivElement>(null);
   const light = useRef<HTMLDivElement>(null);
 
-  const onMouseMove: React.MouseEventHandler<HTMLDivElement> = e => {
-    console.log(e);
+  const onMouseMove: React.MouseEventHandler<HTMLDivElement> = event => {
     if (!card.current) return;
 
-    const {top, left, width, height} = card.current.getBoundingClientRect();
-    const {clientX, clientY} = e;
+    const {width, height} = card.current.getBoundingClientRect();
+    const {offsetX, offsetY} = event.nativeEvent; // 이벤트 핸들러가 등록된 요소로부터 마우스 포인터까지의 상대 거리
+    const [centerX, centerY] = [offsetX - width / 2, offsetY - height / 2];
+    const [tiltX, tiltY] = [-centerY, centerX];
 
-    const centerX = clientX - left - width / 2;
-    const centerY = clientY - top - height / 2;
-    const x = e.nativeEvent.offsetX;
-    const y = e.nativeEvent.offsetY;
-    card.current.style.setProperty('--centerX', `${x}px`);
-    card.current.style.setProperty('--centerY', `${y}px`);
-    const tiltY = centerX;
-    const tiltX = -centerY;
-    const deg =
-      (Math.sqrt(tiltX * tiltX + tiltY * tiltY) /
-        Math.sqrt((width * width) / 4 + (height * height) / 4)) *
-      TILT_DEG;
+    card.current.style.setProperty('--centerX', `${offsetX}px`);
+    card.current.style.setProperty('--centerY', `${offsetY}px`);
+
+    const tiltDegree =
+      (getPytagoras(tiltX, tiltY) / getPytagoras(width / 2, height / 2)) *
+      TILT_DEGREE;
 
     card.current.style.setProperty('--tiltX', String(tiltX));
     card.current.style.setProperty('--tiltY', String(tiltY));
-    card.current.style.setProperty('--deg', `${deg}deg`);
+    card.current.style.setProperty('--tiltDegree', `${tiltDegree}deg`);
 
-    const shadowX = -centerX / 6;
-    const shadowY = -centerY / 6;
+    const [shadowX, shadowY] = [-centerX / 6, -centerY / 6];
     card.current.style.setProperty('--shadowX', `${shadowX}px`);
     card.current.style.setProperty('--shadowY', `${shadowY}px`);
   };
 
+  const onMouseLeave: React.MouseEventHandler<HTMLDivElement> = event => {
+    if (!card.current) return;
+
+    initCssVariables(card.current);
+  };
+
   return (
     <Frame>
-      <Card img={img} ref={card} onMouseMove={onMouseMove}>
+      <Card
+        img={img}
+        ref={card}
+        onMouseMove={onMouseMove}
+        onMouseLeave={onMouseLeave}
+      >
         <Light ref={light} />
       </Card>
     </Frame>
